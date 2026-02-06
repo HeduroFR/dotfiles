36a1146a1442695af07e698d8ce84bc0b1ae4856
HeduroFR <contact@heduro.dev>
2026-02-06 18:54:35 +0100

    Display hidden files in neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		local neo_tree = require("neo-tree")
+		neo_tree.setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false,
+				},
+			},
+		})
+
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { silent = true })
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { silent = true })
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true })
